7df73c58bb95157165407e11073b6f9f254ca25c
Buyaki01 <50216097+Buyaki01@users.noreply.github.com>
2020-05-08 21:38:56 +0300

    Added board.rb, logic.rb and players.rb to the lib folder

--- bin/main.rb
+++ bin/main.rb
@@ -1,36 +1,69 @@
 #!/usr/bin/env ruby
 
-@board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] 
+require_relative '../lib/board.rb'
+require_relative '../lib/players.rb'
+require_relative '../lib/logic.rb'
+
+@board = Board.new
 @winner = ""
 @turn = "X"
 
-def display_board
-  puts "0 |1 |2 "
-  puts "#{@board[0]} |#{@board[1]} |#{@board[2]} "
-  puts "__|__|__"
-  puts "3 |4 |5 "
-  puts "#{@board[3]} |#{@board[4]} |#{@board[5]} "
-  puts "__|__|__"
-  puts "6 |7 |8 "
-  puts "#{@board[6]} |#{@board[7]} |#{@board[8]} "
-  puts "  |  | "
-end
-
 def take_turn(player, cell)
-  @board[cell] = player
+  @board.board[cell] = player
 end
 
 def play
-  while winner = ""
-    display_board
-    puts "Player #{@turn} select your cell between 0 to 8"
+  @board.display_board
+  puts "Player1 write your name"
+  temp_player_name = gets.chomp
+  player1 = Player.new(temp_player_name)
+  puts "player1's name is #{player1.name}"
+  puts ""
+  puts "Player2 write your name"
+  temp_player_name = gets.chomp
+  player2 = Player.new(temp_player_name)
+  puts "Player2's name is #{player2.name}"
+  
+  while @winner == ""
+    @board.display_board
+    if @turn == "X"
+      puts "#{player1.name} select your cell"
+    else
+      puts "#{player2.name} select your cell"
+    end
     cell = gets.chomp
+    
+    while @board.board[cell.to_i] != " "
+      puts "Please only select an empty cell between 0 and 8"
+      puts "Select another cell"
+      cell = gets.chomp
+    end
+
     take_turn(@turn, cell.to_i)
+
+    if check_win(@board.board) == true
+       @winner = @turn
+    end
+
+    if @winner == "" && !@board.board.include?(" ")
+      @winner = "T"
+    end
+
     if @turn == "X"
       @turn = "O"
     else
       @turn = "X"
     end
-  end  
+  end 
+
+  @board.display_board
+  if @winner == "X"
+    puts "#{player1.name} is the winner"
+  elsif @winner == "O"
+    puts "#{player2.name} is the winner"
+  elsif @winner == "T"
+    puts "It's a tie"
+  end
+
 end
 play
