8b979f38e4361cf3ba0c6ccb1990e06d43d8b0d0
Buyaki01 <50216097+Buyaki01@users.noreply.github.com>
2020-05-09 16:27:21 +0300

    Fix the rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -5,8 +5,8 @@ require_relative '../lib/players.rb'
 require_relative '../lib/logic.rb'
 
 @board = Board.new
-@winner = ""
-@turn = "X"
+@winner = ''
+@turn = 'X'
 
 def take_turn(player, cell)
   @board.board[cell] = player
@@ -14,56 +14,51 @@ end
 
 def play
   @board.display_board
-  puts "Player1 write your name"
+  puts 'Player1 write your name'
   temp_player_name = gets.chomp
   player1 = Player.new(temp_player_name)
   puts "player1's name is #{player1.name}"
-  puts ""
-  puts "Player2 write your name"
+  puts ''
+  puts 'Player2 write your name'
   temp_player_name = gets.chomp
   player2 = Player.new(temp_player_name)
   puts "Player2's name is #{player2.name}"
-  
-  while @winner == ""
+
+  while @winner == ''
     @board.display_board
-    if @turn == "X"
+    if @turn == 'X'
       puts "#{player1.name} select your cell"
     else
       puts "#{player2.name} select your cell"
     end
     cell = gets.chomp
-    
-    while @board.board[cell.to_i] != " "
-      puts "Please only select an empty cell between 0 and 8"
-      puts "Select another cell"
+
+    while @board.board[cell.to_i] != ' '
+      puts 'Please only select an empty cell between 0 and 8'
+      puts 'Select another cell'
       cell = gets.chomp
     end
 
     take_turn(@turn, cell.to_i)
 
-    if check_win(@board.board) == true
-       @winner = @turn
-    end
+    @winner = @turn if check_win(@board.board) == true
 
-    if @winner == "" && !@board.board.include?(" ")
-      @winner = "T"
-    end
+    @winner = 'T' if @winner == '' && !@board.board.include?(' ')
 
-    if @turn == "X"
-      @turn = "O"
-    else
-      @turn = "X"
-    end
-  end 
-
-  @board.display_board
-  if @winner == "X"
-    puts "#{player1.name} is the winner"
-  elsif @winner == "O"
-    puts "#{player2.name} is the winner"
-  elsif @winner == "T"
-    puts "It's a tie"
+    @turn = if @turn == 'X'
+              'O'
+            else
+              'X'
+            end
   end
 
+  @board.display_board
+  if @winner == 'X'
+    puts "#{player1.name} is the winner"
+  elsif @winner == 'O'
+    puts "#{player2.name} is the winner"
+  elsif @winner == 'T'
+    puts "It's a tie"
+  end
 end
 play
--- lib/board.rb
+++ lib/board.rb
@@ -1,18 +1,18 @@
 class Board
-    attr_accessor :board
-    def initialize 
-        @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-    end
+  attr_accessor :board
+  def initialize
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+  end
 
-    def display_board
-        puts "0 |1 |2 "
-        puts "#{@board[0]} |#{@board[1]} |#{@board[2]} "
-        puts "__|__|__"
-        puts "3 |4 |5 "
-        puts "#{@board[3]} |#{@board[4]} |#{@board[5]} "
-        puts "__|__|__"
-        puts "6 |7 |8 "
-        puts "#{@board[6]} |#{@board[7]} |#{@board[8]} "
-        puts "  |  | "
-    end
+  def display_board
+    puts '0 |1 |2 '
+    puts "#{@board[0]} |#{@board[1]} |#{@board[2]} "
+    puts '__|__|__'
+    puts '3 |4 |5 '
+    puts "#{@board[3]} |#{@board[4]} |#{@board[5]} "
+    puts '__|__|__'
+    puts '6 |7 |8 '
+    puts "#{@board[6]} |#{@board[7]} |#{@board[8]} "
+    puts '  |  | '
+  end
 end
