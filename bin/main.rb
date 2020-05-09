ce5e578493f8e4e42f84b44b254af84883167762
Buyaki01 <50216097+Buyaki01@users.noreply.github.com>
2020-05-09 16:22:49 +0300

    Fix the rubocop errors

--- bin/main.rb
+++ bin/main.rb
@@ -1,19 +1,19 @@
 #!/usr/bin/env ruby
 
-@board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '] 
-@winner = ""
-@turn = "X"
+@board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+@winner = ''
+@turn = 'X'
 
 def display_board
-  puts "0 |1 |2 "
+  puts '0 |1 |2 '
   puts "#{@board[0]} |#{@board[1]} |#{@board[2]} "
-  puts "__|__|__"
-  puts "3 |4 |5 "
+  puts '__|__|__'
+  puts '3 |4 |5 '
   puts "#{@board[3]} |#{@board[4]} |#{@board[5]} "
-  puts "__|__|__"
-  puts "6 |7 |8 "
+  puts '__|__|__'
+  puts '6 |7 |8 '
   puts "#{@board[6]} |#{@board[7]} |#{@board[8]} "
-  puts "  |  | "
+  puts '  |  | '
 end
 
 def take_turn(player, cell)
@@ -21,16 +21,16 @@ def take_turn(player, cell)
 end
 
 def play
-  while winner = ""
+  while winner == ''
     display_board
     puts "Player #{@turn} select your cell between 0 to 8"
     cell = gets.chomp
     take_turn(@turn, cell.to_i)
-    if @turn == "X"
-      @turn = "O"
-    else
-      @turn = "X"
-    end
-  end  
+    @turn = if @turn == 'X'
+              'O'
+            else
+              'X'
+            end
+  end
 end
 play
