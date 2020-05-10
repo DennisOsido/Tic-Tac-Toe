41a10f51218613ccce20259817f7ddb20c5083aa
Buyaki01 <50216097+Buyaki01@users.noreply.github.com>
2020-05-10 23:53:48 +0300

    Add to the take_turn and play method

--- bin/main.rb
+++ bin/main.rb
@@ -1,36 +1,43 @@
 #!/usr/bin/env ruby
 
-@board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-@winner = ''
-@turn = 'X'
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+intrs_board = Array.new
+(board.length).times do |i|
+  intrs_board[i] = i
+end
+$winner = true
+players = ["p_1", "p_2"]
 
-def display_board
-  puts '0 |1 |2 '
-  puts "#{@board[0]} |#{@board[1]} |#{@board[2]} "
-  puts '__|__|__'
-  puts '3 |4 |5 '
-  puts "#{@board[3]} |#{@board[4]} |#{@board[5]} "
-  puts '__|__|__'
-  puts '6 |7 |8 '
-  puts "#{@board[6]} |#{@board[7]} |#{@board[8]} "
-  puts '  |  | '
+def display_board(board)
+  puts "#{board[0]} |#{board[1]} |#{board[2]} "
+  puts "#{board[3]} |#{board[4]} |#{board[5]} "
+  puts "#{board[6]} |#{board[7]} |#{board[8]} "
 end
 
-def take_turn(player, cell)
-  @board[cell] = player
-end
-
-def play
-  while winner == ''
-    display_board
-    puts "Player #{@turn} select your cell between 0 to 8"
-    cell = gets.chomp
-    take_turn(@turn, cell.to_i)
-    @turn = if @turn == 'X'
-              'O'
-            else
-              'X'
-            end
+def take_turn(board, player, cell)
+  piece = String.new
+  if player == 'p_1'
+    piece = 'X'
+    board[cell] = piece
+  end
+  if player == 'p_2'
+    piece = 'O'
+    board[cell] = piece
   end
 end
-play
+
+def play(board, players)
+  $winner = false
+  while !$winner
+    display_board(board)
+    puts "Player #{players[0]} select your cell between 0 to 8"
+    cell = gets.chomp
+    take_turn(board,players[0], cell.to_i)
+    display_board(board)
+    puts "Player #{players[1]} select your cell between 0 to 8"
+    cell = gets.chomp
+    take_turn(board,players[1], cell.to_i)
+  end
+end
+
+play(board, players)
