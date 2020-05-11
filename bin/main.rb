ac403a5606813b7ec9544dfbae7c0da66c0a7e50
Buyaki01 <50216097+Buyaki01@users.noreply.github.com>
2020-05-11 15:34:48 +0300

    Add check_win and logic method

--- bin/main.rb
+++ bin/main.rb
@@ -1,43 +1,84 @@
 #!/usr/bin/env ruby
 
 board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
-intrs_board = Array.new
-(board.length).times do |i|
+
+# Instruction Board
+intrs_board = []
+board.length.times do |i|
   intrs_board[i] = i
 end
-$winner = true
-players = ["p_1", "p_2"]
+
+players = %w[player1 player2]
 
 def display_board(board)
-  puts "#{board[0]} |#{board[1]} |#{board[2]} "
-  puts "#{board[3]} |#{board[4]} |#{board[5]} "
-  puts "#{board[6]} |#{board[7]} |#{board[8]} "
+  puts "#{board[0]}  |#{board[1]}  |#{board[2]} "
+  puts '_ _|_ _|_ _'
+  puts "#{board[3]}  |#{board[4]}  |#{board[5]} "
+  puts '_ _|_ _|_ _'
+  puts "#{board[6]}  |#{board[7]}  |#{board[8]} "
+  puts '   |   |   '
 end
 
 def take_turn(board, player, cell)
-  piece = String.new
-  if player == 'p_1'
+  if player == 'player1'
     piece = 'X'
     board[cell] = piece
-  end
-  if player == 'p_2'
+  else
     piece = 'O'
     board[cell] = piece
   end
 end
 
 def play(board, players)
-  $winner = false
-  while !$winner
+  winner = true
+  until winner
     display_board(board)
-    puts "Player #{players[0]} select your cell between 0 to 8"
+    puts "#{players[0]} select your cell between 0 to 8"
     cell = gets.chomp
-    take_turn(board,players[0], cell.to_i)
+    take_turn(board, players[0], cell.to_i)
     display_board(board)
-    puts "Player #{players[1]} select your cell between 0 to 8"
+    puts "#{players[1]} select your cell between 0 to 8"
     cell = gets.chomp
-    take_turn(board,players[1], cell.to_i)
+
+    while board[cell.to_i] != ' '
+      puts 'Please only select an empty cell between 0 and 8'
+      puts 'Select another cell'
+      cell = gets.chomp
+    end
+
+    take_turn(board, players[1], cell.to_i)
   end
 end
-
 play(board, players)
+
+def check_win(board, player)
+  puts "#{players[0]} is the winner" if logic(board) == true && player == players[0]
+
+  puts "#{players[1]} is the winner" if logic(board) == true && player == players[1]
+
+  puts "It's a Draw" if !winner && !board.include?(' ')
+end
+
+def logic(board)
+  check_win(board, players)
+  if board[0] == board[1] && board[0] == board[2] && board[0] != ' ' && board[1] != ' ' && board[2] != ' '
+    true
+  elsif board[3] == board[4] && board[3] == board[5] && board[3] != ' ' && board[4] != ' ' && board[5] != ' '
+    true
+  elsif board[6] == board[7] && board[6] == board[8] && board[6] != ' ' && board[7] != ' ' && board[8] != ' '
+    true
+  elsif board[0] == board[3] && board[0] == board[6] && board[0] != ' ' && board[3] != ' ' && board[6] != ' '
+    true
+  elsif board[1] == board[4] && board[1] == board[7] && board[1] != ' ' && board[4] != ' ' && board[7] != ' '
+    true
+  elsif board[2] == board[5] && board[2] == board[8] && board[2] != ' ' && board[5] != ' ' && board[8] != ' '
+    true
+  elsif board[0] == board[4] && board[0] == board[8] && board[0] != ' ' && board[4] != ' ' && board[8] != ' '
+    true
+  elsif board[2] == board[4] && board[2] == board[6] && board[2] != ' ' && board[4] != ' ' && board[6] != ' '
+    true
+  else
+    false
+  end
+end
+#logic(board)
