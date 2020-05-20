73d62217ac2f993566799607ccb9864a799008b5
Buyaki01 <50216097+Buyaki01@users.noreply.github.com>
2020-05-20 10:35:31 +0300

    Add changes to testing.rb

--- spec/testing.rb
+++ spec/testing.rb
@@ -3,28 +3,26 @@ require_relative '../lib/game.rb'
 require_relative '../lib/check_win.rb'
 
 describe Game do
-    describe initialize do
-        it 'testing initialize method in the Game class' do
-            test_game = Game.new(board)
-            test_game.board expect(@board = board).to eql(true)
-        end
+    let(:test_game) { Game.new }
+
+    it 'testing initialize method in the Game class' do
+        expect(test_game.board).to @board = board
     end
 
     describe full? do
         it 'testing full? method in the Game class' do
-            expect(@board.all? { |i| i == 'X' || i == 'O' }).to be_true
+            expect(full?).to eq(@board.all? { |i| i == 'X' || i == 'O' })
         end
     end
 end
 
 describe Win do
-    describe initialize do
-        it 'testing initialize method in the Win class' do
-            test_win = Win.new(board)
-            test_win.board expect(@board = board).to eql(true)
-        end
-    end
+    let(:test_win) { Win.new }
 
+    it 'testing initialize method in the Win class' do
+        expect(test_win.board).to eql(@board = board)
+    end
+    
     describe won? do
         it 'testing won method' do
             won? expect(@board[0] == @board[1] && @board[0] == @board[2] && @board[0] != ' ' && @board[1] != ' ' && @board[2] != ' ').to be_true
