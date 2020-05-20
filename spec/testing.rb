60f38e6da57c21932f413fdac038416643f26b44
Buyaki01 <50216097+Buyaki01@users.noreply.github.com>
2020-05-20 11:39:24 +0300

    Add changes to testing.rb

--- spec/testing.rb
+++ spec/testing.rb
@@ -3,57 +3,60 @@ require_relative '../lib/game.rb'
 require_relative '../lib/check_win.rb'
 
 describe Game do
-    let(:test_game) { Game.new }
-
     it 'testing initialize method in the Game class' do
+        test_game = Game.win(board)
         expect(test_game.board).to @board = board
     end
 
-    describe full? do
-        it 'testing full? method in the Game class' do
-            expect(full?).to eq(@board.all? { |i| i == 'X' || i == 'O' })
-        end
+    it 'testing full? method in the Game class' do
+        test_game = Game.win(board)
+        expect(test_game.full?).to be_true(@board.all? { |i| i == 'X' || i == 'O' })
     end
 end
 
 describe Win do
-    let(:test_win) { Win.new }
-
     it 'testing initialize method in the Win class' do
-        expect(test_win.board).to eql(@board = board)
+        test_win = Win.new(board)
+        expect(test_win.board).to eql(board)
     end
-    
-    describe won? do
-        it 'testing won method' do
-            won? expect(@board[0] == @board[1] && @board[0] == @board[2] && @board[0] != ' ' && @board[1] != ' ' && @board[2] != ' ').to be_true
-        end
-    
-        it 'testing won method' do
-            won? expect(@board[3] == @board[4] && @board[3] == @board[5] && @board[3] != ' ' && @board[4] != ' ' && @board[5] != ' ').to be_true
-        end
-    
-        it 'testing won method' do
-            won? expect(@board[6] == @board[7] && @board[6] == @board[8] && @board[6] != ' ' && @board[7] != ' ' && @board[8] != ' ').to be_true
-        end
-    
-        it 'testing won method' do
-            won? expect(@board[0] == @board[3] && @board[0] == @board[6] && @board[0] != ' ' && @board[3] != ' ' && @board[6] != ' ').to be_true
-        end
-    
-        it 'testing won method' do
-            won? expect(@board[1] == @board[4] && @board[1] == @board[7] && @board[1] != ' ' && @board[4] != ' ' && @board[7] != ' ').to be_true
-        end
-    
-        it 'testing won method' do
-            won? expect(@board[2] == @board[5] && @board[2] == @board[8] && @board[2] != ' ' && @board[5] != ' ' && @board[8] != ' ').to be_true
-        end
-    
-        it 'testing won method' do
-            won? expect(@board[0] == @board[4] && @board[0] == @board[8] && @board[0] != ' ' && @board[4] != ' ' && @board[8] != ' ').to be_true
-        end
-    
-        it 'testing won method' do
-            won? expect(@board[2] == @board[4] && @board[2] == @board[6] && @board[2] != ' ' && @board[4] != ' ' && @board[6] != ' ').to be_true
-        end
+
+    it 'testing won method' do
+        test_win = Win.new(board)
+        expect(test_win.won?).to be_true(@board[0] == @board[1] && @board[0] == @board[2] && @board[0] != ' ' && @board[1] != ' ' && @board[2] != ' ')
+    end
+
+    it 'testing won method' do
+        test_win = Win.new(board)
+        expect(test_win.won?).to be_true(@board[3] == @board[4] && @board[3] == @board[5] && @board[3] != ' ' && @board[4] != ' ' && @board[5] != ' ')
+    end
+
+    it 'testing won method' do
+        test_win = Win.new(board)
+        expect(test_win.won?).to be_true(@board[6] == @board[7] && @board[6] == @board[8] && @board[6] != ' ' && @board[7] != ' ' && @board[8] != ' ')
+    end
+
+    it 'testing won method' do
+        test_win = Win.new(board)
+        expect(test_win.won?).to be_true(@board[0] == @board[3] && @board[0] == @board[6] && @board[0] != ' ' && @board[3] != ' ' && @board[6] != ' ')
+    end
+
+    it 'testing won method' do
+        test_win = Win.new(board)
+        expect(test_win.won?).to be_true(@board[1] == @board[4] && @board[1] == @board[7] && @board[1] != ' ' && @board[4] != ' ' && @board[7] != ' ')
+    end
+
+    it 'testing won method' do
+        test_win = Win.new(board)
+        expect(test_win.won?).to be_true(@board[2] == @board[5] && @board[2] == @board[8] && @board[2] != ' ' && @board[5] != ' ' && @board[8] != ' ')
+    end
+
+    it 'testing won method' do
+        test_win = Win.new(board)
+        expect(test_win.won?).to be_true(@board[0] == @board[4] && @board[0] == @board[8] && @board[0] != ' ' && @board[4] != ' ' && @board[8] != ' ')
+    end
+
+    it 'testing won method' do
+        test_win = Win.new(board)
+        expect(test_win.won?).to be_true(@board[2] == @board[4] && @board[2] == @board[6] && @board[2] != ' ' && @board[4] != ' ' && @board[6] != ' ')
     end
 end
